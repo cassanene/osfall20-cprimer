4fb5bbc64dc9a3f7629459da957fbb41c5207cd5
Cassey Anene <casseyanene@gmail.com>
2020-09-13 14:04:58 -0500

    adding all functions and changing makefile

--- list/list.c
+++ list/list.c
@@ -2,7 +2,7 @@
 //
 // Implementation for linked list.
 //
-// Solomon Lucas / Cassey Anene / Emmanuel Fowora / Chima Egbuji
+// Cassey Anene
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,57 +10,47 @@
 #include "list.h"
 
 //allocating a space in the memory the size of whatever you are putting in the variable list_t and node_t.
-list_t *list_alloc(elem val)
+list_t *list_alloc()
 {
-    //https://www.techiedelight.com/linked-list-implementation-part-1/
-
-    list_t *l = malloc(sizeof(list_t));
-    l->head = malloc(sizeof(node_t));
-    printf("Allocating works \n");
-    l->head->value = val;
-    l->head->next = NULL;
-    printf("The value for this node is %d\n", l->head->value);
-    return l;
+    list_t *t = (list_t *)malloc(sizeof(list_t));
+    return t;
 }
 
 void list_free(list_t *l)
 {
-    node_t *t = l->head;
-    while (t)
+    node_t *node = l->head;
+    while (node != NULL)
     {
-        node_t *temp = t;
-        t = t->next;
+        node_t *temp = node;
+        node = node->next;
         free(temp);
     }
-    free(t);
-    //printf("The value of this is %d \n", l->head->value);
+    free(node);
     return;
 }
 
 void list_print(list_t *l)
 {
+    node_t *t = l->head;
+    while (t)
     {
-
-        node_t *t = l->head;
-        while (t != NULL)
-        {
-            printf("%d ", t->value);
-            t = t->next;
-        }
+        printf("%d \n", t->value);
+        t = t->next;
     }
 }
+
 int list_length(list_t *l)
 {
-    if (!l->head || !l)
-    {
-        return 0;
-    }
-    int length = 1;
     node_t *t = l->head;
+    int length = 0;
 
-    while (t->next)
+    if (t == NULL)
+    { // if there isnt a list
+        return -1;
+    }
+
+    while (t != NULL)
     {
-        printf("Length is now %d \n", length);
         length++;
         t = t->next;
     }
@@ -69,32 +59,188 @@ int list_length(list_t *l)
 
 void list_add_to_back(list_t *l, elem value)
 {
-    node_t *t = l->head;
-    while (t->next)
-    {
-        t = t->next;
-    }
+    node_t *t = malloc(sizeof(node_t)); //allocate space for node t
     t->value = value;
-    printf("The value of the last node is now %d \n", l->head->next->value);
+    t->next = NULL;
+    if (l->head == NULL) // if there is no head
+    {
+        l->head = t; // adding this new node to the front ass the head
+    }
+    else // add it to the last node
+    {
+        node_t *ptr = l->head;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = t;
+    }
 }
+
 void list_add_to_front(list_t *l, elem value)
 {
     node_t *t = l->head;
-
     l->head = (node_t *)malloc(sizeof(node_t));
     l->head->value = value;
     l->head->next = t;
-
-    printf("Successfully added data to the front");
 }
+
 void list_add_at_index(list_t *l, elem value, int index)
 {
+
+    if (index == 0)
+    {
+        list_add_to_front(l, value);
+    }
+
+    node_t *currNode = l->head;
+
+    int i = 0;
+    for (i = 0; i < index - 1; i++)
+    {
+
+        if (currNode->next == NULL)
+        {
+            return;
+        }
+
+        currNode = currNode->next;
+    }
+
+    node_t *t = (node_t *)malloc(sizeof(node_t));
+
+    t->value = value;
+    t->next = currNode->next;
+    currNode->next = t;
 }
 
-elem list_remove_from_back(list_t *l) { return -1; }
-elem list_remove_from_front(list_t *l) { return -1; }
-elem list_remove_at_index(list_t *l, int index) { return -1; }
+elem list_remove_from_back(list_t *l)
+{
 
-bool list_is_in(list_t *l, elem value) { return false; }
-elem list_get_elem_at(list_t *l, int index) { return -1; }
-int list_get_index_of(list_t *l, elem value) { return -1; }
+    elem value;
+    if (l->head == NULL) // if there isnt a list then you cant remove anything
+    {
+        return -1;
+    }
+
+    if (l->head->next == NULL)
+    {
+        value = l->head->value;
+        free(l->head);
+        return value;
+    }
+
+    node_t *currNode = l->head;
+
+    while (currNode->next->next != NULL)
+    {
+        currNode = currNode->next;
+    }
+
+    value = currNode->next->value;
+    free(currNode->next);
+    currNode->next = NULL;
+    return value;
+}
+
+elem list_remove_from_front(list_t *l)
+{
+    elem value;
+    if (l->head == NULL)
+    {
+        return -1;
+    }
+
+    node_t *t = l->head->next;
+    value = l->head->value;
+    free(l->head);
+    l->head = t;
+
+    return value;
+}
+
+elem list_remove_at_index(list_t *l, int index)
+{
+    elem value;
+    if (index == 0)
+    {
+        return list_remove_from_front(l);
+    }
+
+    node_t *currNode = l->head;
+    node_t *t = NULL;
+
+    int i = 0;
+    for (i = 0; i < index - 1; i++)
+    {
+
+        if (currNode->next == NULL)
+        {
+            return -1;
+        }
+        currNode = currNode->next;
+    }
+
+    t = currNode->next;
+    value = t->value;
+    currNode->next = t->next;
+    free(t);
+
+    return value;
+}
+
+bool list_is_in(list_t *l, elem value)
+{
+
+    node_t *currNode = l->head;
+
+    while (currNode != NULL)
+    {
+
+        if (currNode->value == value)
+        {
+            return true;
+        }
+        currNode = currNode->next;
+    }
+
+    return false;
+}
+
+elem list_get_elem_at(list_t *l, int index)
+{
+
+    node_t *currNode = l->head;
+
+    for (int i = 0; i < index; i++)
+    {
+
+        if (currNode->next == NULL)
+        {
+            return -1;
+        }
+        currNode = currNode->next;
+    }
+
+    return currNode->value;
+}
+
+int list_get_index_of(list_t *l, elem value)
+{
+
+    int i = 0;
+    node_t *currNode = l->head;
+
+    while (currNode != NULL)
+    {
+
+        if (currNode->value == value)
+        {
+            return i;
+        }
+        i++;
+        currNode = currNode->next;
+    }
+
+    return -1;
+}
--- list/main.c
+++ list/main.c
@@ -5,8 +5,42 @@
 int main()
 {
     printf("Tests for linked list implementation\n");
-    list_t *test = list_alloc(5);
-    list_add_to_back(test, 7);
-    list_free(test);
+    printf("Allocating memory...\n");
+    list_t *list_one = list_alloc(5);
+    printf("Adding elements to list...\n");
+    list_add_to_back(list_one, 3);
+    list_add_to_front(list_one, 4);
+    list_add_to_front(list_one, 1);
+    list_add_to_back(list_one, 6);
+    list_add_to_back(list_one, 8);
+    list_add_to_back(list_one, 7);
+    list_add_to_back(list_one, 9);
+    list_add_to_back(list_one, 12);
+    list_add_to_back(list_one, 15);
+    list_add_at_index(list_one, 10, 1);
+    list_print(list_one);
+    int length = list_length(list_one);
+    printf("Length of the list is %d\n", length);
+    printf("Removing from back...\n");
+    list_remove_from_back(list_one);
+    list_print(list_one);
+    printf("Removing from front...\n");
+    list_remove_from_front(list_one);
+    list_print(list_one);
+    printf("Removing at index 2...\n");
+    list_remove_at_index(list_one, 2);
+    list_print(list_one);
+    printf("Checking if 11 is in list...\n");
+    bool isin11 = list_is_in(list_one, 11);
+    printf("%s\n", isin11 ? "true" : "false");
+    printf("Checking if 4 is in list...\n");
+    bool isin4 = list_is_in(list_one, 4);
+    printf("%s\n", isin4 ? "true" : "false");
+    elem element = list_get_elem_at(list_one, 3);
+    printf("Element at index 3 is %d\n", element);
+    printf("Getting index of element 9...\n");
+    int index = list_get_index_of(list_one, 9);
+    printf("%d\n", index);
+
     return 0;
 }
